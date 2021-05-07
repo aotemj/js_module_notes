97d47ce399197527cfc20fa6923c8ee3c5512e73
lovixk <991807272@qq.com>
2021-05-07 17:47:31 +0800

    feature: CommonJS 服务端实现

---
+++ js_module_test/common_js/node/app.js
@@ -0,0 +1,8 @@
+const module1 = require('./module/module1')
+const module2 = require('./module/module2')
+const module3 = require('./module/module3')
+
+module1.foo()
+module2()
+module3.foo()
+module3.bar()
---
+++ js_module_test/common_js/node/module/module3.js
@@ -0,0 +1,7 @@
+exports.foo = function () {
+    console.log('module3 foo invoked');
+}
+
+exports.bar = function () {
+    console.log('module3 bar invoked');
+}
