2b6ac9a4bf6021a60247fb6673f3e47ba091f0ef
lovixk <991807272@qq.com>
2021-05-08 14:54:19 +0800

    feature: CMD 代码实现（seajs）

---
+++ js_module_test/CMD/modules/module2.js
@@ -0,0 +1,10 @@
+// 有依赖其他模块定义
+define(function (require, exports, module) {
+    let msg = 'module2';
+
+    function bar() {
+        console.log(msg)
+    }
+
+    module.exports = bar;
+})
---
+++ js_module_test/CMD/modules/module3.js
@@ -0,0 +1,9 @@
+define(function (require, exports, module) {
+    let data = 'module3';
+
+    function fun() {
+        console.log(data)
+    }
+
+    exports.moudle3 = {fun}
+})
